6ec91e17732e8e617688ef3b4232e1a67c9c822e
Alex Sutton <mandarinfish27@aol.com>
2023-09-26 15:52:14 -0600

    closed final comment on Tech Company Stacks.js

---
+++ Tech Company Stacks.js
@@ -0,0 +1,43 @@
+//The only guidelines for this document were the ones on the rubric, and I wasn't sure it's looking for specific languages (i.e. Java, Python, a MERN stack, etc.) or descriptions of the technologies. Adobe and Microsoft only give more vague info anyway.
+
+
+/* Adobe:
+        Adobe describes their stack as "five functional layers", which are:
+            -Cloud Infrastructure -- they list either AWS or Azure for IaaS foundations
+            -Platform as a Service -- Adobe commerce provides PaaS environments for testing and integrating services
+            -Adobe Commerce -- this provides a "pre-provisioned infrastructure" that includes PHP, SQL, and other database and search technologies.
+            -Performance Tools -- this layer provides debugging. monitoring, and management tools for the application(s)
+            -Content Delivery Network -- This is a featureset that includes CDN, and includes DDoS Protection, PCI Compliance, and Image Optimization
+
+Microsoft Azure:
+        Since Microsoft has a ton of different stacks listed on their website, I'll focus on Azure specifically:
+            -Azure is a set of cloud services offered by Microsoft for building, testing, deploying, and managing applications and services in the cloud. There are many different
+            facets to Azure, but the most known are:
+                -Compute Services -- Azure provides a set of compute services for building, testing, and deploying applications. These include:
+                    -Virtual Machines (VMs) -- the physical servers that run applications
+                    -Databases
+                    -Storage
+                -Networking -- a set of networking services for connecting your applications to the internet
+                -Identity and Access Management -- a set of "access management services" for managing user access to applications
+                -Deployment -- Azure also provides a set of deployment services for deploying applications
+
+
+Spotify:
+        Spotify actually has a super cool backend that uses a lot of nifty tricks to work as well as it does.
+            -Backend
+                Spotify's backend is focused on microservices, which allows for huge scalability. Spotify lists a bunch of different tech they use, including:
+                    - Java
+                    - Scala
+                    - Node.js
+                    - Apache Kafka
+                    - Apache Cassandra
+                    - Redis
+                    - Docker
+            -Frontend
+                Spotify's frontend mainly uses web tech, including:
+                    - React
+                    - Redux
+                    - Sass
+                    - Webpack
+            -Other
+                Spotify engineers also created "Scio" which is a Scala API for Apache Beam that is now a popular open source tech used by many people. */
