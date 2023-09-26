b8ab6154fe1d10942086735ae221bae850e08f0b
Alex Sutton <mandarinfish27@aol.com>
2023-09-26 14:49:51 -0600

    orders.js completion

--- orders.js
+++ orders.js
@@ -29,8 +29,19 @@
     stored on the object.
 */
 
-//CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId) {
+        this.items = items
+        this.orderTime = orderTime
+        this.customerId = customerId
+        this.status = 'queued'
+    }
 
+    updateStatus(newStatus) {
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}`)
+    }
+}
 
 
 /*
@@ -44,7 +55,7 @@
     customer: 575
 */
 
-//CODE HERE
+let firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 PM', 575)
 
 
 /*
@@ -53,4 +64,4 @@
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus('cooking')
